5e6f9a801d3c3a702ccb2cf91250d6ddd0af1907
Kamal Kumar <kamal.nitians@gmail.com>
2025-07-26 23:27:04 +0530

    providing  backend deploy link in TrascriptInput.jsx for full stack

--- frontend/src/components/TranscriptInput.jsx
+++ frontend/src/components/TranscriptInput.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import axios from 'axios';
 
 
-const BASE_URL="https://fastapi-backend-vpxy.onrender.com"
+const BASE_URL="https://neural-ninjas-meeting-bot1.onrender.com"
 function TranscriptInput() {
   const [file, setFile] = useState(null);
   const [transcript, setTranscript] = useState('');
